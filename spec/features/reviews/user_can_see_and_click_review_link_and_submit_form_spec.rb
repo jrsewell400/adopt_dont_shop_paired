4adad29c8a49305a72323516f0c31b2a08ce4094
Jordan Sewell <jrsewell400@gmail.com>
2020-02-04 16:30:57 -0700

    Add shelter_id to  review creation and update test expectations

--- spec/features/reviews/user_can_see_and_click_review_link_and_submit_form_spec.rb
+++ spec/features/reviews/user_can_see_and_click_review_link_and_submit_form_spec.rb
@@ -14,16 +14,34 @@ RSpec.describe "As a visitor", type: :feature do
                         title: "Good Shelter",
                         rating: 5,
                         content: "Fantastic staff and cleanily area for animals.",
-                        picture: "https://i.pinimg.com/474x/8b/f4/d4/8bf4d4c3062e2f1b719d2b9c22b671ab--dog-boarding-kennels-dog-kennels.jpg"
+                        picture: "https://i.pinimg.com/474x/8b/f4/d4/8bf4d4c3062e2f1b719d2b9c22b671ab--dog-boarding-kennels-dog-kennels.jpg",
+                        shelter_id: shelter_1.id
                         )
     
       visit "/shelters/#{shelter_1.id}"
 
-      click_link "Create New Review"                  
-    
-      expect(page).to have_link("Create New Review")
+      click_link "Create New Review"
+   
       expect(current_path).to eq("/shelters/#{shelter_1.id}/new")
-      expect(page).to have_content("Create New Review")
+      expect(page).to have_content("Create New Review:")
+      expect(page).to have_content("Title")
+      expect(page).to have_content("Rating")
+      expect(page).to have_content("Content")
+      expect(page).to have_content("Picture")
+
+      fill_in 'title', with: 'Best shelter ever!'
+      fill_in 'rating', with: 5
+      fill_in 'content', with: 'Staff was friendly and they take great care of the pets!'
+      fill_in 'picture', with: 'https://www.rd.com/wp-content/uploads/2017/10/These-Funny-Dog-Videos-Are-the-Break-You-Need-Right-Now_493370860-Jenn_C-760x506.jpg'
+      
+      click_on("Create New Review")
+
+      have_current_path "/shelters/#{shelter_1.id}"
+      
+      expect(page).to have_content("Best shelter ever!")
+      expect(page).to have_content('5')
+      expect(page).to have_content('Staff was friendly and they take great care of the pets!')
+      expect(page).to have_content('https://www.rd.com/wp-content/uploads/2017/10/These-Funny-Dog-Videos-Are-the-Break-You-Need-Right-Now_493370860-Jenn_C-760x506.jpg')
     end
   end
 end
