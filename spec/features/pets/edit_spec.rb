b822294ba5b30c5a6959df5774017aad3359540d
Jordan Sewell <jrsewell400@gmail.com>
2020-02-08 12:17:01 -0700

    Comment out  adoption expectations and remove user story comment, also changed submitter to Submit for link click

--- spec/features/pets/edit_spec.rb
+++ spec/features/pets/edit_spec.rb
@@ -22,13 +22,13 @@ RSpec.describe "edit pets", type: :feature do
     fill_in 'description', with: 'Little White Dog'
     fill_in 'age', with: '909'
     fill_in 'sex', with: 'Male'
-    fill_in 'adopted', with: 'Pending Adoption'
-    click_on('submitter')
+    # fill_in 'adopted', with: 'Pending Adoption'
+    click_on('Submit')
     expect(page).to have_content('Astro')
     expect(page).to have_content('909')
     expect(page).to have_content('Little White Dog')
     expect(page).to have_content('Male')
-    expect(page).to have_content('Pending Adoption')
+    # expect(page).to have_content('Pending Adoption')
   end
   it "can edit pets from pet index page" do
     shelter_1 = Shelter.create(name:       "This is a Shelter",
@@ -58,30 +58,12 @@ RSpec.describe "edit pets", type: :feature do
     fill_in 'description', with: 'Little White Dog'
     fill_in 'age', with: '909'
     fill_in 'sex', with: 'Male'
-    fill_in 'adopted', with: 'Pending Adoption'
-    click_on('submitter')
+    # fill_in 'adopted', with: 'Pending Adoption'
+    click_on('Submit')
     expect(page).to have_content('Astro')
     expect(page).to have_content('909')
     expect(page).to have_content('Little White Dog')
     expect(page).to have_content('Male')
-    expect(page).to have_content('Pending Adoption')
+    # expect(page).to have_content('Pending Adoption')
   end
 end
-
-
-#     User Story 11, Pet Update
-#
-# As a visitor
-# When I visit a Pet Show page
-# Then I see a link to update that Pet "Update Pet"
-# When I click the link
-# I am taken to '/pets/:id/edit' where I see a form to edit the pet's data including:
-# - image
-# - name
-# - description
-# - approximate age
-# - sex
-# When I click the button to submit the form "Update Pet"
-# Then a `PATCH` request is sent to '/pets/:id',
-# the pet's data is updated,
-# and I am redirected to the Pet Show page where I see the Pet's updated information
