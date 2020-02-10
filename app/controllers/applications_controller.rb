77142dd84e716c6fec2ef7869d8749c57f24e054
Chase.Gaddis <cgaddis36@gmail.com>
2020-02-09 18:44:06 -0700

    Finished functionality for user story 17

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationsController < ApplicationController
 
   def new
+    @pets = Pet.all
+    @pet_favorites = Array.new
+    @pets.each do |pet|
+      if session[:favorites] && session[:favorites].has_key?(pet.id.to_s)
+        @pet_favorites << pet
+      end
+    end
   end
 
   def create
