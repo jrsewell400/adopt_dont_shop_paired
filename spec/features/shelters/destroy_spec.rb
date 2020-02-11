b8143ba1bb6ba0986431b3afcf8dab614f1a4203
Jordan Sewell <jrsewell400@gmail.com>
2020-02-10 19:43:12 -0700

    Add before each block

--- spec/features/shelters/destroy_spec.rb
+++ spec/features/shelters/destroy_spec.rb
@@ -1,33 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "shelter show page", type: :feature do
-  it "can update shelters" do
-    shelter_1 = Shelter.create!(name:       "Anotha Shelta",
-                         address:       "1042 N Marion St",
-                         city:          "Denver",
-                         state:         "Colorado",
-                          zip:          "80218")
+  before :each do 
+    @shelter_1 = Shelter.create(name: "This is a Shelter",
+                                address: "1042 N Marion St",
+                                city: "Denver",
+                                state: "Colorado",
+                                zip: "80218"
+                               )
 
+    @review1 = Review.create(title: "Good Shelter",
+                             rating: 5,
+                             content: "Fantastic staff and cleanily area for animals.",
+                             picture: "https://i.pinimg.com/474x/8b/f4/d4/8bf4d4c3062e2f1b719d2b9c22b671ab--dog-boarding-kennels-dog-kennels.jpg",
+                             shelter_id: @shelter_1.id
+                            )
+  end
+  
+  it "can update shelters" do
     visit '/shelters'
     click_on('Delete This Shelter')
     have_current_path "/shelters"
-    expect(page).to_not have_content('Anotha Shelta')
+    expect(page).to_not have_content('This is a Shelter')
   end
 
   it "can delete individual shelters" do
-    shelter_1 = Shelter.create(name:       "This is a Shelter",
-                         address:       "1042 N Marion St",
-                         city:          "Denver",
-                         state:         "Colorado",
-                          zip:          "80218")
-
-    review1 = Review.create(
-                          title: "Good Shelter",
-                          rating: 5,
-                          content: "Fantastic staff and cleanily area for animals.",
-                          picture: "https://i.pinimg.com/474x/8b/f4/d4/8bf4d4c3062e2f1b719d2b9c22b671ab--dog-boarding-kennels-dog-kennels.jpg",
-                          shelter_id: shelter_1.id
-                          )
     visit "/shelters"
     expect(page).to have_content('This is a Shelter')
     click_on("Delete This Shelter")
