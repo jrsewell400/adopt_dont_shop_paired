dc71432496eabca3fe58cf933c1f93604b9b77d1
Jordan Sewell <jrsewell400@gmail.com>
2020-02-10 19:42:10 -0700

    Fix line spacing and formatting

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "shelter show page", type: :feature do
   it "can update shelters" do
-    shelter_1 = Shelter.create!(name:       "Anotha Shelta",
-                                address:       "1042 N Marion St",
-                                city:          "Denver",
-                                state:         "Colorado",
-                                zip:          "80218")
-    review1 = Review.create(
-                            title: "Good Shelter",
+    shelter_1 = Shelter.create(name: "Anotha Shelta",
+                               address: "1042 N Marion St",
+                               city: "Denver",
+                               state: "Colorado",
+                               zip: "80218"
+                              )
+
+    review1 = Review.create(title: "Good Shelter",
                             rating: 5,
                             content: "Fantastic staff and cleanily area for animals.",
                             picture: "https://i.pinimg.com/474x/8b/f4/d4/8bf4d4c3062e2f1b719d2b9c22b671ab--dog-boarding-kennels-dog-kennels.jpg",
                             shelter_id: shelter_1.id
-                            )
+                           )
 
   visit "/shelters/#{shelter_1.id}"
   click_link 'Edit This Review'
@@ -22,9 +23,7 @@ RSpec.describe "shelter show page", type: :feature do
   fill_in 'content', with: 'Edited Review Content'
   fill_in 'rating', with: '0'
   fill_in 'picture', with: 'https://s3.amazonaws.com/lowres.cartoonstock.com/sport-apocalypse-apocalyptic-dead_worlds-dying_worlds-free_time-gra110412_low.jpg'
-
   click_on('Submit')
-
   have_current_path "/shelters/#{shelter_1.id}"
   expect(page).to have_content('Edited Review Title')
   expect(page).to have_content('Edited Review Content')
