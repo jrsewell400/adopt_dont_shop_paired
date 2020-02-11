444133120a156fa4e87d6986b9548e35f5e9c3be
Jordan Sewell <jrsewell400@gmail.com>
2020-02-10 19:41:14 -0700

    Fix within test block

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe "shelters index page", type: :feature do
                                zip: "80218")
 
     lilly = Pet.create!(image: "pets/golden.jpg",
-                       name: "Lilly",
-                       description: "Black Dog",
-                       age: "4",
-                       sex: "Female",
-                       shelter_id: shelter_1.id)
+                        name: "Lilly",
+                        description: "Black Dog",
+                        age: "4",
+                        sex: "Female",
+                        shelter_id: shelter_1.id)
 
     visit "/shelters"
     click_on shelter_1.name
@@ -24,7 +24,6 @@ RSpec.describe "shelters index page", type: :feature do
     expect(page).to have_content('Black Dog')
     expect(page).to have_content('4')
     expect(page).to have_content('Female')
-    # expect(page).to have_content('Adoptable')
   end
 
   it "shows all pets in the database" do
@@ -44,12 +43,12 @@ RSpec.describe "shelters index page", type: :feature do
 
     visit "/pets"
     have_current_path "/pets"
-    # within "#pet-#{lilly.id}" do
+    within "#pet-#{lilly.id}" do
       expect(page).to have_content('Lilly')
       expect(page).to have_content('4')
       expect(page).to have_content('Female')
       expect(page).to have_content('Generic Shelter')
       expect(page).to have_css("img[src*='#{lilly.image}']")
-    #end
+    end
   end
 end
