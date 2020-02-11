79c934a0e88c7da4772a7dd5158f80bfbeb7c7a9
Jordan Sewell <jrsewell400@gmail.com>
2020-02-10 19:41:23 -0700

    Fix formatting

--- spec/features/reviews/create_flash_spec.rb
+++ spec/features/reviews/create_flash_spec.rb
@@ -2,20 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "New Reviews pages", type: :feature do
   it "receives a flash message when you create a review successfully and unsuccessfully" do
-    shelter_1 = Shelter.create(
-                         name:       "Shelters 'r Us",
-                         address:       "1042 N Marion St",
-                         city:          "Denver",
-                         state:         "Colorado",
-                         zip:          "80218")
+    shelter_1 = Shelter.create(name: "Shelters 'r Us",
+                               address: "1042 N Marion St",
+                               city: "Denver",
+                               state: "Colorado",
+                               zip: "80218"
+                              )
 
-    review1 = Review.create(
-                        title: "Good Shelter",
-                        rating: 5,
-                        content: "Fantastic staff and cleanily area for animals.",
-                        picture: "https://i.pinimg.com/474x/8b/f4/d4/8bf4d4c3062e2f1b719d2b9c22b671ab--dog-boarding-kennels-dog-kennels.jpg",
-                        shelter_id: shelter_1.id
-                        )
+    review1 = Review.create(title: "Good Shelter",
+                            rating: 5,
+                            content: "Fantastic staff and cleanily area for animals.",
+                            picture: "https://i.pinimg.com/474x/8b/f4/d4/8bf4d4c3062e2f1b719d2b9c22b671ab--dog-boarding-kennels-dog-kennels.jpg",
+                            shelter_id: shelter_1.id
+                           )
 
     visit "/shelters/#{shelter_1.id}/new"
     fill_in 'title', with: 'Best shelter ever!'
