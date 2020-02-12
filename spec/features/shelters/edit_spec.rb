b4a7373e9256a61ba08fae1d56425959289e4ea1
Chase.Gaddis <cgaddis36@gmail.com>
2020-02-11 17:31:32 -0700

    user story 29 complete.

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "shelter show page", type: :feature do
-  before :each do  
+  before :each do
     @shelter_1 = Shelter.create!(name: "Anotha Shelta",
                                  address: "1042 N Marion St",
                                  city: "Denver",
                                  state: "Colorado",
                                  zip: "80218"
                                 )
-                                
+
     @review1 = Review.create(title: "Good Shelter",
                              rating: 5,
                              content: "Fantastic staff and cleanily area for animals.",
@@ -35,24 +35,20 @@ RSpec.describe "shelter show page", type: :feature do
     expect(page).to have_content('Edited Shelter Zip')
   end
 
-  it "can update shelters" do
+  it "if I try to edit a shelter and fail to fill out a form I see that forms attributes that I failed to fill out" do
     visit '/shelters'
-    have_current_path "/shelters "
-    click_link 'Anotha Shelta'
-    have_current_path "/shelters/#{@shelter_1.id}"
     click_link 'Edit This Shelter'
     have_current_path "/shelters/#{@shelter_1.id}/edit"
     fill_in 'name', with: 'Edited Shelter Name'
     fill_in 'address', with: 'Edited Shelter Address'
     fill_in 'city', with: 'Edited Shelter City'
     fill_in 'state', with: 'Edited Shelter State'
-    fill_in 'zip', with: 'Edited Shelter Zip'
+    fill_in 'zip', with: ''
     click_on('Submit')
-    have_current_path "/shelters/#{@shelter_1.id}"
-    expect(page).to have_content('Edited Shelter Name')
-    expect(page).to have_content('Edited Shelter Address')
-    expect(page).to have_content('Edited Shelter City')
-    expect(page).to have_content('Edited Shelter State')
-    expect(page).to have_content('Edited Shelter Zip')
+    expect(page).to have_content("Zip can't be blank")
   end
 end
+
+# When I am updating or creating a new shelter
+# If I try to submit the form with incomplete information
+# I see a flash message indicating which field(s) I am missing
